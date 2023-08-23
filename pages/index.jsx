480ec638586dd179493d75585fde9344aa6842fd
kodachi-gsd <kodachi@gsd-ltd.co.jp>
2023-08-23 16:27:07 +0900

    コミット

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,8 @@ import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
   const [count, setCount] = useState(1)
+  const [text, setText] = useState("")
+  const [isShow, setIsShow] = useState(true)
 
   const handleClick = useCallback(() => {
     console.log(count);
@@ -27,9 +29,21 @@ export default function Home() {
       // console.log(`アンマウント時: ${count}`)
       document.body.style.backgroundColor = "";
     };
-  },[count]);
+  },[]);
 
-  console.log(count)
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  },[]);
+
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
 
   return (
     <div className={styles.container}>
@@ -39,8 +53,18 @@ export default function Home() {
       </Head>
       
       <Header />
-        <h1>{count}</h1>
+
+        {isShow ? <h1>{count}</h1> : null}
         <button onClick={handleClick}>button</button>
+        <button onClick={handleDisplay}>
+          {isShow ? "非表示" : "表示"}
+        </button>
+
+        <input 
+          type="text" 
+          value={text} 
+          onChange={handleChange}
+        />
       <Main page="index" />
       <Footer />
     </div>
