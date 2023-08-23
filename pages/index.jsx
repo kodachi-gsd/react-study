614b43577056c7bb1c354fbfe78216282ab12b6d
kodachi-gsd <kodachi@gsd-ltd.co.jp>
2023-08-23 11:44:12 +0900

    コミット

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,18 @@ import styles from 'styles/Home.module.css'
 import { Footer } from 'components/Footer'
 import { Main } from 'components/Main'
 import { Header } from 'components/Header'
+import { useCallback } from 'react'
+
 
 export default function Home() {
+  const foo = 1
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +23,12 @@ export default function Home() {
       </Head>
       
       <Header />
+      <a
+        href="/about"
+        onClick={handleClick}
+      >
+        button
+      </a>
       <Main page="index" />
       <Footer />
     </div>
